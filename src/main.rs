c89ee62ab9ffca96c231db42ae49e9022f53ae63
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-02-02 17:03:50 +0200

    Initial playing with args and executable name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::env;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let exe = env::current_exe()?;
+    println!("Current executable: {:?}", exe);
+    let cfg = exe.with_extension("toml");
+    println!("Current cfg:        {:?}", cfg);
+    println!("Args:");
+    for arg in env::args() {
+        println!("+ {}", arg);
+    }
+    Ok(())
 }
