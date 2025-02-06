0c1e59cad22de7a503f0a8294a962edd32550b49
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-02-06 22:36:23 +0200

    printing more paths for diagnostics

--- wexe/src/main.rs
+++ wexe/src/main.rs
@@ -21,9 +21,11 @@ fn get_wexe_cfg_dir() -> PathBuf {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let wexe_cfg_dir = get_wexe_cfg_dir();
+    println!("Central Config directory: {:?}", wexe_cfg_dir);
     let exe = env::current_exe()?;
     println!("Current executable: {:?}", exe);
     let toml_name = exe.with_extension("toml");
+    println!("Local Config file: {:?}", toml_name);
     let cfg_naked = toml_name.file_name().unwrap();
     let cfg_file = wexe_cfg_dir.join(cfg_naked);
     println!("App config file is: {:?}", cfg_file);
