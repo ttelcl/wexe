498e6c14d965972289d85176b7343e121e7a3967
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-03-01 22:49:40 +0200

    Initial stub for CommandCollection

--- wexe/src/bin/wexecfg/commands.rs
+++ wexe/src/bin/wexecfg/commands.rs
@@ -1,7 +1,8 @@
-
 #![allow(dead_code)]
 
+use std::collections::{BTreeMap, HashMap};
 use std::error::Error;
+use wexe::console_colors::*;
 
 pub trait Command {
     /// The primary name of the command. Defaults to the first item
@@ -15,4 +16,50 @@ pub trait Command {
     /// usually 0 for success, 1 for soft failure (e.g. after showing a help
     /// message instead of actually doing something).
     fn execute(&self, args: &Vec<&str>) -> Result<i32, Box<dyn Error>>;
+
+    fn print_help(&self) {
+        println!("Help for command: {stl_i}{fg_o}{}{rst}", self.name());
+    }
+}
+
+pub struct CommandCollection {
+    /// The commands in this collection, indexed and ordered by their primary name.
+    commands: BTreeMap<String, Box<dyn Command>>,
+
+    /// A map of all command names and aliases to their primary names.
+    command_map: HashMap<String, String>,
+}
+
+impl CommandCollection {
+    /// Create a new, empty command collection.
+    pub fn new() -> CommandCollection {
+        CommandCollection {
+            commands: BTreeMap::new(),
+            command_map: HashMap::new(),
+        }
+    }
+
+    /// Add a command to the collection (moving the command into it).
+    pub fn add_command(&mut self, command: Box<dyn Command>) {
+        // First set up the command map, before moving the command into 'commands'.
+        for name in command.name_and_aliases() {
+            self.command_map
+                .insert(name.to_string(), command.name().to_string());
+        }
+        self.commands.insert(command.name().to_string(), command);
+    }
+
+    /// Get a command by name or alias.
+    pub fn get_command(&self, name: &str) -> Option<&Box<dyn Command>> {
+        match self.command_map.get(name) {
+            Some(primary_name) => self.commands.get(primary_name),
+            None => None,
+        }
+    }
+
+    /// Return a list of all commands in this collection as a newly allocated vector.
+    /// The commands are ordered by their primary name.
+    pub fn get_commands(&self) -> Vec<&Box<dyn Command>> {
+        self.commands.values().collect()
+    }
 }
--- wexe/src/bin/wexecfg/main.rs
+++ wexe/src/bin/wexecfg/main.rs
@@ -1,7 +1,21 @@
 use wexe::console_colors::*;
 
+use commands::CommandCollection;
+
 mod commands;
+
+fn setup_commands() -> CommandCollection {
+    #[allow(unused_mut)]
+    let mut commands = CommandCollection::new();
+    // commands.add_command(Box::new(commands::HelpCommand::new()));
+    // commands.add_command(Box::new(commands::ListCommand::new()));
+    // commands.add_command(Box::new(commands::SetCommand::new()));
+    // commands.add_command(Box::new(commands::UnsetCommand::new()));
+    commands
+}
 
 fn main() {
     println!("Hello, {fg_g}wexecfg{rst}! ({stl_i}{stl_d}Not yet implemented{rst})");
+    #[allow(unused_variables)]
+    let commands = setup_commands();
 }
