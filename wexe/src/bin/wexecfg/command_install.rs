fe4050c458627949854a9772b3a63731b10966e5
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-03-08 17:26:14 +0200

    wexecfg /install: now implemented.

--- wexe/src/bin/wexecfg/command_install.rs
+++ wexe/src/bin/wexecfg/command_install.rs
@@ -1,7 +1,11 @@
 #![allow(unused_imports)]
 
+use std::env;
 use std::error::Error;
+use std::fs;
 use std::process::ExitCode;
+
+use same_file::is_same_file;
 
 use super::args_buffer::ArgumentsBuffer;
 use super::commands::{Command, CommandCollection};
@@ -66,9 +70,44 @@ impl Command for InstallCommand {
             commands.print_help_for(self.name());
             return Err(format!("Invalid arguments for command '{}'.", self.name()).into());
         }
+        let exe = env::current_exe().expect("Could not get the current executable path.");
+        let current_exe_folder = exe
+            .parent()
+            .expect("Could not get the executable's parent folder.");
+        let repo = WexeRepository::new();
+        let cfg_folder = repo.get_config_folder();
 
-        //let repo = WexeRepository::new();
-        panic!("{fg_o}Not implemented yet: /install{rst}.");
-        //Ok(ExitCode::SUCCESS)
+        if is_same_file(&current_exe_folder, &cfg_folder).unwrap() {
+            println!(
+                "{fg_r}Error!{fg_o} The current exe folder is the same as the config folder{rst}. \
+                \n{fg_y}Run {fg_o}wexecfg /install{fg_y} from the directory where it was built{rst}."
+            );
+            return Err("Current exe folder is the same as the config folder.".into());
+        }
+
+        let wexecfg_dest = repo.get_wexecfg_exe_path();
+        println!(
+            "Copying {fg_b}{:}{fg_W} to {fg_g}{:}{rst}.",
+            &exe.to_string_lossy(),
+            wexecfg_dest.to_string_lossy()
+        );
+        fs::copy(&exe, wexecfg_dest)?;
+
+        let wexe_dest = repo.get_wexe_exe_path();
+        if options.include_wexe {
+            println!(
+                "Copying {fg_b}{:}{fg_W} to {fg_g}{:}{rst}.",
+                &exe.to_string_lossy(),
+                wexe_dest.to_string_lossy()
+            );
+            fs::copy(exe, wexe_dest)?;
+        } else {
+            println!(
+                "{fg_o}{}{fg_y} not copied. Use {fg_g}-wexe{fg_y} to include it{rst}.",
+                &exe.to_string_lossy()
+            );
+        }
+
+        Ok(ExitCode::SUCCESS)
     }
 }
